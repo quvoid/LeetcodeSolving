2efcda007e878294278f4a447ba2799d54d99b26
Omkar Rakshe <138476505+quvoid@users.noreply.github.com>
2025-10-30 18:38:56 +0530

    Time: 7 ms (62.38%) | Memory: 47.8 MB (71.51%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -3,12 +3,13 @@ class Solution {
 
         Map<String, List<String>> worded = new HashMap<>();
 
+        
         for(String s : strs){
-            char[] chars =  s.toCharArray();
-            Arrays.sort(chars);
-            String sorted = new String(chars);
+            char[] chars =  s.toCharArray();    //convert the string to character
+            Arrays.sort(chars);                            //sort the character
+            String sorted = new String(chars);  
 
-            if(!worded.containsKey(sorted)){
+            if(!worded.containsKey(sorted)){        //first aet comes , it is checked if aet is in the hashmap , if not it is added aet as key then eat which is the sort of aet is added , then tea enters , which is sorted to aet , aet is checked , it is already present so tea is directly added at line 16 through add(s)
                 worded.put(sorted, new ArrayList<>());
             }
 
