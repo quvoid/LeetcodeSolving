252de14cf73b777c48c01887a5b173a8e4651740
Omkar Rakshe <138476505+quvoid@users.noreply.github.com>
2025-10-30 10:30:50 +0530

    Time: 5 ms (55.36%) | Memory: 42.5 MB (99.65%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -4,14 +4,14 @@ class Solution {
             return false;
         }
 
-        int[] charlen = new int[26];
+        int[] charlen = new int[26];        //creates a 26 boxes for storing the alphabets 
         for(int i = 0;i<s.length();i++){
-            charlen[s.charAt(i) - 'a']++;
-            charlen[t.charAt(i) - 'a']--;
+            charlen[s.charAt(i) - 'a']++;   //plus any character on i by 1 
+            charlen[t.charAt(i) - 'a']--;   //minus any character on i by 1
         }
 
         for(int count : charlen){
-            if(count != 0){
+            if(count != 0){                 //(-1+1 = 0 ) everything in array should be zero 
                 return false;
             }
         }  
